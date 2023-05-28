dc33a5e73f2ad6e1f4142c956cba1a2fb7897d27
Ajay-baburaj <96784826+Ajay-baburaj@users.noreply.github.com>
2023-05-28 22:51:52 +0530

    Delete helper directory

--- helper/dateFormat.js
+++
@@ -1,7 +0,0 @@
-
-
-module.exports.getFormattedDate = async(bookingDate) => {
-    const date = new Date(bookingDate);
-    const formattedDate =date.toLocaleDateString('en-US', { weekday: 'short', month: 'short', day: 'numeric', year: 'numeric' });
-    return formattedDate
-}
--- helper/idGenerator.js
+++
@@ -1,9 +0,0 @@
-const { v4: uuidv4 } = require('uuid');
-
-module.exports.generateUniqueRandomNumber=()=> {
-  const uuid = uuidv4();
-  const random_number = uuid.substr(0, 6).replace(/-/g, '');
-    return random_number;
-}
-
-
