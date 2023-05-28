6ca59f4b1521e6df6557ab833f648c69c9977f25
ajay baburaj <ajaybaburajp@gmail.com>
2023-05-29 00:03:14 +0530

    cors

--- index.js
+++ index.js
@@ -9,10 +9,12 @@ const cookieParser = require("cookie-parser");
 require('dotenv').config();
 const app = express()
 
-app.use(cors({
-    origin: ['http://localhost:3000','http://localhost:3001','http://localhost:3002'],
-    credentials: true
-}));
+// app.use(cors({
+//     origin: ['http://localhost:3000','http://localhost:3001','http://localhost:3002'],
+//     credentials: true
+// }));
+
+app.use(cors())
 
 
 
